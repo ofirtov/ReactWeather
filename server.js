ed266111d468f8d192848f4a43e2fc8198dbab6f
ofir tov <ofir.tov@gmail.com>
2017-02-20 12:34:44 +0200

    Update middleware for localhost

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const PORT = process.env.PORT || 3000; //use node enviroment port OR port 3000 (
 
 //! To redirect https to http essential to use the openWeatherMap API
 app.use(function(req, res, next) {
-		if (req.headers['x-forwarded-proto'] === 'http'){
-			next();
+		if (req.headers['x-forwarded-proto'] === 'https'){
+			res.redirect('http://' + req.hostname + req.url);
 		}
 		else
 		{
-			res.redirect('http://' + req.hostname + req.url);
+			next();
 		}
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-	console.log('Express server is up on port 3000' + PORT);
+	console.log('Express server is up on port ' + PORT);
 });
